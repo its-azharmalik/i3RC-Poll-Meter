8728c451df13275596686da10b49bf06ff5209e2
svnitad <svnitad@gmail.com>
2022-03-02 00:08:46 +0530

    minor

--- Schemas/User.js
+++ Schemas/User.js
@@ -1,65 +1,83 @@
-const mongoose = require('mongoose');
-const {authType} = require('../Utils')
+const mongoose = require("mongoose");
+const { authType } = require("../Utils");
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type : String ,
-        required : true ,
-        // unique : true 
-    }, 
-    email : {
-        type : String ,
-        required : true ,
-        unique : true 
+  name: {
+    type: String,
+    required: true,
+    // unique : true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  authType: {
+    type: String,
+    enum: authType,
+    required: true,
+  },
+  party: {
+    type: String,
+  },
+  lok_sabha_access: [
+    {
+      type: String,
     },
-    password : {
-        type : String ,
-        required : true
-    },
-    authType : {
-        type : String,
-        enum : authType ,
-        required : true
-    },
-    party : {
-        type : String,
-    },
-    lok_sabha_access : [{
-        type : String ,
-    }],
-    vidhan_sabha_access : [{
-        state:{
-            type: String,
-            // required:true,
+  ],
+  vidhan_sabha_access: [
+    {
+      state: {
+        type: String,
+        // required:true,
+      },
+      vdns: [
+        {
+          type: Number,
         },
-        vdns :[{
-            type : Number,
-        }]
-    }],
-    ward_no_access : [{
-        state : {type: String},
-        cities :[{
-            city : {
-                type : String,
+      ],
+    },
+  ],
+  ward_no_access: [
+    {
+      state: { type: String },
+      cities: [
+        {
+          city: {
+            type: String,
+          },
+          wdns: [
+            {
+              type: String,
             },
-            wdns : [{
-                type : Number
-            }]
-        }],
-    }],
-    Data_Collector_access : [{
-        state : {type: String},
-        cities :[{
-            city : {
-                type : String,
+          ],
+        },
+      ],
+    },
+  ],
+  Data_Collector_access: [
+    {
+      state: { type: String },
+      cities: [
+        {
+          city: {
+            type: String,
+          },
+          pbns: [
+            {
+              type: Number,
             },
-            pbns :[{
-                type : Number
-            }]
-        }],
-    }],
-})
+          ],
+        },
+      ],
+    },
+  ],
+});
 
-const User = mongoose.model("User" ,userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
