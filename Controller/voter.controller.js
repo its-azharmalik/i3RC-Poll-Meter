e9269735170602365012274fb71bd3f8a665c4ab
svnitad <svnitad@gmail.com>
2022-03-29 19:02:00 +0530

    ff

--- Controller/voter.controller.js
+++ Controller/voter.controller.js
@@ -47,109 +47,112 @@ const getAllVoterdata = async (req, res) => {
   }
 };
 
-const getAllVoterDataLkn = async (req,res) =>{
+const getAllVoterDataLkn = async (req, res) => {
   try {
     const q = req.query;
     const keys = Object.keys(q);
-    if(keys.length === 0){
+    if (keys.length === 0) {
       res.status(200).json({
-        note:"NO DATA AVAILABLE QUERIES REQUIRED"
-      })
-    }
-    else{
+        note: "NO DATA AVAILABLE QUERIES REQUIRED",
+      });
+    } else {
       const allVoterData = await Voter.find({});
-      let final_data =[];
+      let final_data = [];
       keys.forEach((key) => {
-        let  datas = allVoterData;
+        let datas = allVoterData;
         const queryLkn = parseInt(q[key]);
-        final_data.push(datas.filter(function(data) {
-          return data.Upload_data.Lok_Sabha_Number === queryLkn;
-        }));
+        final_data.push(
+          datas.filter(function (data) {
+            return data.Upload_data.Lok_Sabha_Number === queryLkn;
+          })
+        );
       });
       res.json({
-        note : "succes",
-        final_data
-      })
+        note: "succes",
+        final_data,
+      });
     }
   } catch (error) {
     res.status(400).json({
       note: "error",
-      error
-    })
+      error,
+    });
   }
-}
+};
 
-const getAllVoterDataVdn = async (req,res) =>{
+const getAllVoterDataVdn = async (req, res) => {
   try {
     const q = req.query;
     const state = q.state;
     const keys = Object.keys(q);
-    if(keys.length === 0){
+    if (keys.length === 0) {
       res.status(200).json({
-        note:"NO DATA AVAILABLE QUERIES REQUIRED"
-      })
-    }
-    else{
-      const allVoterData = await Voter.find({
-        'Upload_data.state' : state
+        note: "NO DATA AVAILABLE QUERIES REQUIRED",
       });
-      let final_data =[];
+    } else {
+      const allVoterData = await Voter.find({
+        "Upload_data.state": state,
+      });
+      let final_data = [];
       keys.forEach((key) => {
-        let  datas = allVoterData;
+        let datas = allVoterData;
         const queryVdn = parseInt(q[key]);
-        final_data.push(datas.filter(function(data) {
-          return data.Upload_data.Vidhan_Sabha_Number === queryVdn;
-        }));
+        final_data.push(
+          datas.filter(function (data) {
+            return data.Upload_data.Vidhan_Sabha_Number === queryVdn;
+          })
+        );
       });
       res.json({
-        note : "succes",
-        final_data
-      })
+        note: "succes",
+        final_data,
+      });
     }
   } catch (error) {
     res.status(400).json({
       note: "error",
-      error
-    })
+      error,
+    });
   }
-}
+};
 
-const getAllVoterDataWdn = async (req,res) =>{
+const getAllVoterDataWdn = async (req, res) => {
   try {
     const q = req.query;
     const state = q.state;
     const city = q.city;
     const keys = Object.keys(q);
-    if(keys.length === 0){
+    if (keys.length === 0) {
       res.status(200).json({
-        note:"NO DATA AVAILABLE QUERIES REQUIRED"
-      })
-    }
-    else{
-      const allVoterData = await Voter.find({
-        'Upload_data.state' : state,
-        'Upload_data.city' : city,
+        note: "NO DATA AVAILABLE QUERIES REQUIRED",
       });
-      let final_data =[];
+    } else {
+      const allVoterData = await Voter.find({
+        "Upload_data.state": state,
+        "Upload_data.city": city,
+      });
+      let final_data = [];
       keys.forEach((key) => {
-        let  datas = allVoterData;
+        let datas = allVoterData;
         const queryWdn = parseInt(q[key]);
-        final_data.push(datas.filter(function(data) {
-          return data.Upload_data.Ward_No === queryWdn;
-        }));
+        final_data.push(
+          datas.filter(function (data) {
+            return data.Upload_data.Ward_No === queryWdn;
+          })
+        );
       });
       res.json({
-        note : "succes",
-        final_data
-      })
+        note: "succes",
+        final_data,
+      });
     }
   } catch (error) {
     res.status(400).json({
       note: "error",
-      error
-    })
+      error,
+    });
   }
-}
+};
 
 const getVoterdata = async (req, res) => {
   try {
